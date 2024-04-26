aef8b59023050f272fff3094af8f71cce5f495e7
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2024-04-26 20:37:14 +0000

    remove success page

--- success.php
+++
@@ -1,6 +0,0 @@
-<?php require('partials/header.php') ?>
-
-<h1>Votre devoir a été enregistré avec succès !</h1>
-<a href="index.php">Retour à l'accueil</a>
-
-<?php require('partials/footer.php') ?>
